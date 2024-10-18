521feb36bcd4acfd443c30c3e4a6415b2013332e
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-10-18 21:45:28 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// Your code here
+function initialize () {
+    
+    fetch("http://localhost:3000/films/1")
+    .then(res => res.json())
+    .then(movie => {
+        const poster = document.getElementById("poster")
+        poster.src = movie.poster
+        const title = document.getElementById("title")
+        title.textContent = movie.title
+        const runtime = document.getElementById("runtime")
+        runtime.textContent = `Runtime:${movie.runtime} minutes`
+        const description = document.getElementById("film-info") 
+        description.textContent = movie.description
+        const showtime = document.getElementById("showtime")
+        showtime.textContent = movie.showtime
+        const availableTickets = document.getElementById("ticket-num")
+        availableTickets.textContent = movie.capacity - movie.tickets_sold
+        console.log(movie)
+    })
+}
+
+document.addEventListener('DOMContentLoaded', initialize)
